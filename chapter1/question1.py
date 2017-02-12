ac3268c432ab2d503a0416b2bf16366dc43accb0
colinwd <me@colinwd.com>
2017-02-12 09:53:24 -0800

    question one solution

--- chapter1/question1.py
+++ chapter1/question1.py
@@ -1,2 +1,13 @@
 # Implement an algorithm to determine if a string has all unique characters.
 # What if you cannot use additional data structures?
+
+
+def unique_characters(input):
+    seen_characters = []
+    for letter in input:
+        if letter in seen_characters:
+            return False
+        else:
+            seen_characters.append(letter)
+
+    return True
